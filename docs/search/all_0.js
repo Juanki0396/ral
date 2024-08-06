e9b845498a8d203677dcb6e17e504f3a1e55ddcf
Juanki0396 <Juanki0396@users.noreply.github.com>
2024-08-06 00:35:18 +0000

    deploy: 0821f223753d54fce7628aa3756b3e53b4ab7bdf

--- docs/search/all_0.js
+++ docs/search/all_0.js
@@ -7,7 +7,7 @@ var searchData=
   ['get_5flikelihood_4',['get_likelihood',['../namespacek4_1_1ral_1_1ParticleID.html#ac002f63ea183addf99cfab09b8cbf189',1,'k4::ral::ParticleID']]],
   ['get_5fmass_5',['get_mass',['../namespacek4_1_1ral_1_1ReconstructedParticle.html#a33d2feca4c5b3e2029778e203eadcd52',1,'k4::ral::ReconstructedParticle']]],
   ['get_5fmomentum_6',['get_momentum',['../namespacek4_1_1ral_1_1ReconstructedParticle.html#a1890f7ec86017b9e0a35ceabb5609a1c',1,'k4::ral::ReconstructedParticle']]],
-  ['get_5fpdg_7',['get_pdg',['../namespacek4_1_1ral_1_1ParticleID.html#afa04332136834231ec266cf256f04c70',1,'k4::ral::ParticleID::get_PDG()'],['../namespacek4_1_1ral_1_1ReconstructedParticle.html#a3a3c0cbf80f11df3ff092673ddaafb1a',1,'k4::ral::ReconstructedParticle::get_PDG(ROOT::VecOps::RVec&lt; edm4hep::ReconstructedParticleData &gt; particles)']]],
+  ['get_5fpdg_7',['get_PDG',['../namespacek4_1_1ral_1_1ParticleID.html#afa04332136834231ec266cf256f04c70',1,'k4::ral::ParticleID::get_PDG()'],['../namespacek4_1_1ral_1_1ReconstructedParticle.html#a3a3c0cbf80f11df3ff092673ddaafb1a',1,'k4::ral::ReconstructedParticle::get_PDG(ROOT::VecOps::RVec&lt; edm4hep::ReconstructedParticleData &gt; particles)']]],
   ['get_5freferencepoint_8',['get_referencePoint',['../namespacek4_1_1ral_1_1ReconstructedParticle.html#acc55bf5d888e7ad7f957a2a7c018902f',1,'k4::ral::ReconstructedParticle']]],
   ['get_5ftype_9',['get_type',['../namespacek4_1_1ral_1_1ParticleID.html#a7e9b4cf06ba211f8111b7c34a639b59b',1,'k4::ral::ParticleID']]]
 ];
