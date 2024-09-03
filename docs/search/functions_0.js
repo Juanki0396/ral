0cb76d8ccbda65eb22e5bdaccebfd15c832f20f5
Juanki0396 <Juanki0396@users.noreply.github.com>
2024-09-03 09:14:01 +0000

    deploy: 3daed389d9c26d3ad1d1076549756df4ecbcd528

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['filter_0',['filter',['../namespacek4_1_1ral_1_1LogicalOperators.html#a6d307e2cf0e17b865e10899e555a7001',1,'k4::ral::LogicalOperators']]],
-  ['filter_3c_20edm4hep_3a_3amcparticledata_20_3e_1',['filter&lt; edm4hep::MCParticleData &gt;',['../namespacek4_1_1ral_1_1LogicalOperators.html#ae5aed9955744f54d6440969c2bf8f1d6',1,'k4::ral::LogicalOperators']]],
-  ['filter_3c_20edm4hep_3a_3areconstructedparticledata_20_3e_2',['filter&lt; edm4hep::ReconstructedParticleData &gt;',['../namespacek4_1_1ral_1_1LogicalOperators.html#a262a218d84d0875e07eb76196218c8c5',1,'k4::ral::LogicalOperators']]]
+  ['filter_0',['filter',['../namespacek4_1_1ral_1_1LogicalOperators.html#a8818d7487f067b8e0bffc92ba19fc38a',1,'k4::ral::LogicalOperators::filter(const ROOT::VecOps::RVec&lt; bool &gt; &amp;mask, const ROOT::VecOps::RVec&lt; T &gt; &amp;collection)'],['../namespacek4_1_1ral_1_1LogicalOperators.html#ad4b2e17006f93ef6db5f1ec16be90c62',1,'k4::ral::LogicalOperators::filter(const ROOT::VecOps::RVec&lt; bool &gt; &amp;mask, const C &amp;collection)']]]
 ];
