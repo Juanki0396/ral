9505ddf7d028c7708543562537ab4568185a4c4a
Juanki0396 <Juanki0396@users.noreply.github.com>
2024-09-05 18:59:36 +0000

    deploy: eaa2802b69cf9c49e699cfec73aa218a16352e66

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -2,11 +2,13 @@ var searchData=
 [
   ['k4_0',['k4',['../namespacek4.html',1,'']]],
   ['k4_3a_3aral_1',['ral',['../namespacek4_1_1ral.html',1,'k4']]],
-  ['k4_3a_3aral_3a_3acluster_2',['Cluster',['../namespacek4_1_1ral_1_1Cluster.html',1,'k4::ral']]],
-  ['k4_3a_3aral_3a_3alogicaloperators_3',['LogicalOperators',['../namespacek4_1_1ral_1_1LogicalOperators.html',1,'k4::ral']]],
-  ['k4_3a_3aral_3a_3amcparticle_4',['MCParticle',['../namespacek4_1_1ral_1_1MCParticle.html',1,'k4::ral']]],
-  ['k4_3a_3aral_3a_3aparticleid_5',['ParticleID',['../namespacek4_1_1ral_1_1ParticleID.html',1,'k4::ral']]],
-  ['k4_3a_3aral_3a_3areconstructedparticle_6',['ReconstructedParticle',['../namespacek4_1_1ral_1_1ReconstructedParticle.html',1,'k4::ral']]],
-  ['k4_3a_3aral_3a_3atrack_7',['Track',['../namespacek4_1_1ral_1_1Track.html',1,'k4::ral']]],
-  ['k4_3a_3aral_3a_3avertex_8',['Vertex',['../namespacek4_1_1ral_1_1Vertex.html',1,'k4::ral']]]
+  ['k4_3a_3aral_3a_3acalorimeterhit_2',['CalorimeterHit',['../namespacek4_1_1ral_1_1CalorimeterHit.html',1,'k4::ral']]],
+  ['k4_3a_3aral_3a_3acluster_3',['Cluster',['../namespacek4_1_1ral_1_1Cluster.html',1,'k4::ral']]],
+  ['k4_3a_3aral_3a_3alogicaloperators_4',['LogicalOperators',['../namespacek4_1_1ral_1_1LogicalOperators.html',1,'k4::ral']]],
+  ['k4_3a_3aral_3a_3amcparticle_5',['MCParticle',['../namespacek4_1_1ral_1_1MCParticle.html',1,'k4::ral']]],
+  ['k4_3a_3aral_3a_3aparticleid_6',['ParticleID',['../namespacek4_1_1ral_1_1ParticleID.html',1,'k4::ral']]],
+  ['k4_3a_3aral_3a_3areconstructedparticle_7',['ReconstructedParticle',['../namespacek4_1_1ral_1_1ReconstructedParticle.html',1,'k4::ral']]],
+  ['k4_3a_3aral_3a_3atrack_8',['Track',['../namespacek4_1_1ral_1_1Track.html',1,'k4::ral']]],
+  ['k4_3a_3aral_3a_3atrackerhit3d_9',['TrackerHit3D',['../namespacek4_1_1ral_1_1TrackerHit3D.html',1,'k4::ral']]],
+  ['k4_3a_3aral_3a_3avertex_10',['Vertex',['../namespacek4_1_1ral_1_1Vertex.html',1,'k4::ral']]]
 ];
--- docs/search/files_0.js
+++ docs/search/files_0.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['cluster_2ecc_0',['Cluster.cc',['../Cluster_8cc.html',1,'']]],
-  ['cluster_2eh_1',['Cluster.h',['../Cluster_8h.html',1,'']]]
+  ['calorimeterhit_2ecc_0',['CalorimeterHit.cc',['../CalorimeterHit_8cc.html',1,'']]],
+  ['calorimeterhit_2eh_1',['CalorimeterHit.h',['../CalorimeterHit_8h.html',1,'']]],
+  ['cluster_2ecc_2',['Cluster.cc',['../Cluster_8cc.html',1,'']]],
+  ['cluster_2eh_3',['Cluster.h',['../Cluster_8h.html',1,'']]]
 ];
